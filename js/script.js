f3726ecf70f7becc25f7a224165fd50ac44988c3
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-23 22:57:33 +0900

    style: text-box 스타일 추가

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// PARAM splittype 라인 옵션
+const lineUpSplitOption = {
+  types: 'lines, words',
+  tagName: 'span',
+  lineClass: 'line-wrap',
+  wordClass: 'word',
+};
+
+const splitedText = [new SplitType('.text-eng', lineUpSplitOption)];
