64c27f800b77e95c8af21f4ffd203922af81115d
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-28 00:35:52 +0900

    feat: 텍스트 모션 추가

--- js/motion.js
+++ js/motion.js
@@ -15,39 +15,70 @@ const setHeaderFix = () => {
   });
 };
 
-//  FUNCTION
+// FUNCTION fade-up 되는 텍스트
+const setFadeUpMotion = (selector) => {
+  const tl = gsap.timeline({
+    repeatRefresh: true,
+  });
+  tl.to(selector, {
+    y: () => 0,
+    opacity: 1,
+    duration: 1,
+  });
+
+  return tl;
+};
+
+//  FUNCTION word들이 약간의 시간차를 두고 올라오는 모션
 const setWordUpMotion = (selector) => {
   // selector: string
-  const wordList = gsap.utils.toArray(`${selector} .word`);
-  const lineList = gsap.utils.toArray(`${selector} .line`);
-  gsap.set(wordList, { y: () => 100 });
-  gsap.to(wordList, {
-    y: () => 0,
-    duration: 1,
-    stagger: 0.01,
+  const tl = gsap.timeline({
+    repeatRefresh: true,
     scrollTrigger: {
       trigger: selector,
       start: () => `top center`,
       markers: true,
       invalidateOnRefresh: true,
     },
-    onComplete: () => {
-      el.style.overflow = 'unset';
-    },
   });
 
-  // ScrollTrigger.create({
-  //   trigger: selector,
-  //   start: () => `top center`,
-  //   markers: true,
-  // });
+  const wordList = gsap.utils.toArray(`${selector} .word`);
+  tl.set(wordList, { y: () => 100 });
+  // text-eng의 word들을 시차를 두고 fade-up
+  tl.to(wordList, {
+    y: () => 0,
+    duration: 1,
+    stagger: 0.03,
+    // onComplete: () => {
+    //   el.style.overflow = 'unset';
+    // },
+  });
 
-  console.log(wordList);
+  const nextEl = document.querySelector(selector).nextElementSibling;
+  console.log(nextEl.classList);
+  if (nextEl && nextEl.classList.contains('text-ko')) {
+    // 다음 dom이 존재하고, 다음 dom에 text-ko 클래스가 존재하면
+    tl.set(nextEl, { y: () => 100, opacity: 0 });
+    tl.add(setFadeUpMotion(nextEl));
+  }
 };
+// PARAM setWordUpMotion을 적용할 리스트
+// TODO before 부분에도 motion 추가될 수 있게 수정해야함.
+const wordUpList = [
+  `[data-area='1'] [data-text='1'] .text-eng`,
+  `[data-area='1'] [data-text='2'] .text-eng`,
+  `[data-area='1'] [data-text='3'] .text-eng`,
+  `[data-area='4'] [data-text='2'] .text-eng`,
+  `[data-area='12'] [data-text='1'] .text-eng`,
+  `[data-area='12'] [data-text='2'] .text-eng`,
+];
 
 // FUNCTION motion load
 window.addEventListener('load', () => {
   console.log('motion load');
   setHeaderFix();
-  setWordUpMotion(`[data-area='1'] [data-text='1'] .text-eng`);
+
+  wordUpList.forEach((el) => {
+    setWordUpMotion(el);
+  });
 });
