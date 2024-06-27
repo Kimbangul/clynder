1adadae00ffdb8b07cae2ef9439639c1b0ba4442
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-27 19:17:21 +0900

    feat: word 올라오는 모션, header fix 기능 추가

--- js/motion.js
+++ js/motion.js
@@ -1,28 +1,53 @@
+// FUNCTION pc header 고정시키기
 const setHeaderFix = () => {
   const header = document.querySelector('.header__nav');
 
   ScrollTrigger.create({
     trigger: header,
-    markers: true,
-    endTrigger: document.querySelector('body'),
-    end: () => `bottom top`,
     invalidateOnRefresh: true,
-    onToggle: (self) => console.log(self),
-    // onUpdate: (self) => {
-    //     console.log(
-    //         'progress:',
-    //         self.progress.toFixed(3),
-    //         'direction:',
-    //         self.direction,
-    //         'velocity',
-    //         self.getVelocity()
-    //     );
-    // }
+    start: () => `-=2rem start`,
+    onEnter: () => {
+      header.classList.add('fix');
+    },
+    onLeaveBack: () => {
+      header.classList.remove('fix');
+    },
   });
+};
+
+//  FUNCTION
+const setWordUpMotion = (selector) => {
+  // selector: string
+  const wordList = gsap.utils.toArray(`${selector} .word`);
+  const lineList = gsap.utils.toArray(`${selector} .line`);
+  gsap.set(wordList, { y: () => 100 });
+  gsap.to(wordList, {
+    y: () => 0,
+    duration: 1,
+    stagger: 0.01,
+    scrollTrigger: {
+      trigger: selector,
+      start: () => `top center`,
+      markers: true,
+      invalidateOnRefresh: true,
+    },
+    onComplete: () => {
+      el.style.overflow = 'unset';
+    },
+  });
+
+  // ScrollTrigger.create({
+  //   trigger: selector,
+  //   start: () => `top center`,
+  //   markers: true,
+  // });
+
+  console.log(wordList);
 };
 
 // FUNCTION motion load
 window.addEventListener('load', () => {
   console.log('motion load');
   setHeaderFix();
+  setWordUpMotion(`[data-area='1'] [data-text='1'] .text-eng`);
 });
