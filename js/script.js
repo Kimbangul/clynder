53ab92cd63fa925fa07d5126d8ffe9238431483c
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-06-27 11:26:01 +0900

    feat: 모바일 메뉴 여닫기

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// PARAM selector
+const mbOpenBtn = document.querySelector('.menu-btn');
+
 // PARAM splittype 라인 옵션
 const lineUpSplitOption = {
   types: 'lines, words',
@@ -5,5 +8,22 @@ const lineUpSplitOption = {
   lineClass: 'line-wrap',
   wordClass: 'word',
 };
-
 const splitedText = [new SplitType('.text-eng', lineUpSplitOption)];
+
+// FUNCTION 모바일에서 메뉴 버튼 여닫기
+const setOpenMenu = () => {
+  // isOpen: boolean
+  const menuWrap = document.querySelector('.menu-wrap');
+  const isOpenMbMenu = menuWrap.getAttribute('data-open');
+  const isOpen = JSON.parse(isOpenMbMenu);
+
+  menuWrap.setAttribute('data-open', !isOpen);
+  mbOpenBtn.setAttribute('data-open', !isOpen);
+};
+
+// FUNCTION After window load
+window.addEventListener('load', () => {
+  console.log('load');
+
+  document.querySelector('.menu-btn').addEventListener('click', setOpenMenu);
+});
